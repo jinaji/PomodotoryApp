13551b4d8920f46d632c660035536893de6853ba
jina <jinkim2@student.42seoul.kr>
2023-12-14 20:44:28 +0900

    feat: 색 추가

--- Pomodotory.tsx
+++ Pomodotory.tsx
@@ -1,9 +1,39 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export const Pomodotory = () => {
   return (
-    <View>
-      <Text>Pomodotory</Text>
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: styles.dotory.color,
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View style={{ backgroundColor: styles.highlight.color }}>
+        <Text style={styles.white}>Pomodotory</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  dotory: {
+    color: "#B5815B",
+  },
+  subColor: {
+    color: "#F4C58D",
+  },
+  highlight: {
+    color: "#FAB754",
+  },
+  white: {
+    color: "#fff",
+  },
+});
